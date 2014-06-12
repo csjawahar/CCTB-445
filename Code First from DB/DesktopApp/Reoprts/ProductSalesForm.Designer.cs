6b89b193cfa07b72159e16feb455f4212aca474f
Jawahar <csjawahar@yahoo.com>
2014-06-12 12:56:12 -0600

    Reports work

---
+++ Code First from DB/DesktopApp/Reoprts/ProductSalesForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace DesktopApp.Reoprts
+{
+    partial class ProductSalesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.ProductSaleSummaryBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductSaleSummaryBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ProductSaleSummaryBindingSource
+            // 
+            this.ProductSaleSummaryBindingSource.DataSource = typeof(NorthWindSystem.Entities.POCOS.ProductSaleSummary);
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            reportDataSource1.Name = "ProductSalesDataset";
+            reportDataSource1.Value = this.ProductSaleSummaryBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "DesktopApp.Reoprts.ProductSalesReport.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(12, 42);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(791, 265);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // ProductSalesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 408);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "ProductSalesForm";
+            this.Text = "ProductSalesForm";
+            this.Load += new System.EventHandler(this.ProductSalesForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ProductSaleSummaryBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource ProductSaleSummaryBindingSource;
+    }
+}
---
+++ Code First from DB/DesktopApp/Reoprts/ProductSalesForm.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NorthWindSystem.Entities.POCOS;
+using NorthwindSystem.BLL;
+using NorthwindSystem.Entities;
+
+namespace DesktopApp.Reoprts
+{
+    public partial class ProductSalesForm : Form
+    {
+        public ProductSalesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ProductSalesForm_Load(object sender, EventArgs e)
+        {
+            List<ProductSaleSummary> list = new NorthwindManager().GetProductSaleSummaries();
+            reportViewer1.LocalReport.DataSources.Clear();
+
+            reportViewer1.LocalReport.ReportEmbeddedResource = "DesktopApp.Reoprts.ProductSalesReport.rdlc";
+
+            Microsoft.Reporting.WinForms.ReportDataSource dataset = new Microsoft.Reporting.WinForms.ReportDataSource("ProductSalesDataset", list);
+            reportViewer1.LocalReport.DataSources.Add(dataset);
+            dataset.Value = list;
+            reportViewer1.LocalReport.Refresh();
+            this.reportViewer1.RefreshReport();
+        }
+
+      
+    }
+}
